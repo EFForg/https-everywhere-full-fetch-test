b480af03b4cf33ba36c04291adbde0d15f6065eb
Alexis Hancock <lxshancock@gmail.com>
2020-01-28 15:19:48 -0800

    pdate fetch test docker environment
    - Pull properly for Python 3 (some packages resolved to 2.7)
    - Update maintainer label

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 git clone --depth=1 https://github.com/EFForg/https-everywhere.git /opt
 cd /opt
 find rules/ -type f -exec sed -i "s/ default_off='failed ruleset test'//g" {} \;
-python test/rules/src/https_everywhere_checker/check_rules.py test/rules/disable-broken-rulesets.checker.config
+python3.6 test/rules/src/https_everywhere_checker/check_rules.py test/rules/disable-broken-rulesets.checker.config
 git diff > /patch/diff.patch
